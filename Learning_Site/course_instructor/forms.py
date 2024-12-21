92a819128cf24e306190a1a79418498cab500d9f
Mudassir Latif <mudassirm.latif@gmail.com>
2024-12-21 15:22:14 +0500

    ho gaya

---
+++ Learning_Site/course_instructor/forms.py
@@ -0,0 +1,69 @@
+from django import forms
+from course_instructor.models import Course, CourseContent
+from .models import Quiz, Question
+
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['title', 'description', 'course_type', 'duration', 'price']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter course title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 1, 'placeholder': 'Enter course description'}),
+            'course_type': forms.Select(attrs={'class': 'form-control'}),
+            'duration': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Duration in hours'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter course price'}),
+        }
+
+class CourseContentForm(forms.ModelForm):
+    class Meta:
+        model = CourseContent
+        fields = ['Contenttitle', 'video', 'content_type', 'order', 'additional_resources', 'is_last_video']
+        widgets = {
+            'Contenttitle': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter content title'}),
+            'video': forms.FileInput(attrs={'class': 'form-control'}),
+            'content_type': forms.Select(attrs={'class': 'form-control'}),
+            'order': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter content order'}),
+            'additional_resources': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Enter additional resources'}),
+            'is_last_video': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+class QuizForm(forms.ModelForm):
+    class Meta:
+        model = Quiz
+        exclude = ['title']
+        widgets = {
+            'question_text': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter question text'}),
+            'option_a': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter option A'}),
+            'option_b': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter option B'}),
+            'option_c': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter option C'}),
+            'option_d': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter option D'}),
+            'correct_answer': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['question_text', 'option_a', 'option_b', 'option_c', 'option_d', 'correct_answer']
+        widgets = {
+            'question_text': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter question text'}),
+            'option_a': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter option A'}),
+            'option_b': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter option B'}),
+            'option_c': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter option C'}),
+            'option_d': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter option D'}),
+            'correct_answer': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+from django import forms
+from .models import CourseFeedback
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = CourseFeedback
+        fields = ['video_quality', 'audio_quality', 'teaching_method', 'course_content', 'feedback']
+        widgets = {
+            'video_quality': forms.Select(attrs={'class': 'form-control'}),
+            'audio_quality': forms.Select(attrs={'class': 'form-control'}),
+            'teaching_method': forms.Select(attrs={'class': 'form-control'}),
+            'course_content': forms.Select(attrs={'class': 'form-control'}),
+            'feedback': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Your feedback here...'}),
+        }
---
+++ Learning_Site/student/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Student
+
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['full_name', 'email', 'date_of_birth', 'course_name', 'type', 'payment_method', 'payment_screenshot']
+        widgets = {
+            'full_name': forms.TextInput(attrs={'class': 'form-control'}),  # Added widget for Full Name
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),     # Added widget for Email
+            'date_of_birth': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'course_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'type': forms.Select(attrs={'class': 'form-control'}),
+            'payment_method': forms.Select(attrs={'class': 'form-control'}),
+            'payment_screenshot': forms.FileInput(attrs={'class': 'form-control'}),
+        }
